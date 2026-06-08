5a6f79881075803239c8a1bbb7c99418f362f894
= <=>
2026-06-08 13:50:06 -0400

    added sloppy wavedashing

--- Game_Assets/Objects/subaru.js
+++ Game_Assets/Objects/subaru.js
@@ -3,51 +3,92 @@ export default class Player extends Phaser.Physics.Arcade.Sprite {
         super(scene,x,y,'subaru');
         scene.add.existing(this);
         scene.physics.add.existing(this);
-        this.x = x;
-        this.y = y;
-        this.keys = scene.input.keyboard.addKeys('UP,LEFT,RIGHT');
-        this.setOrigin(0.5,0.9);
+        this.keys = scene.input.keyboard.addKeys('UP,LEFT,RIGHT,SHIFT');
 
         //movement variables
         this.topSpeed = 105;
-        this.jumpHeight = -100;
         this.acceleration = 10;
         this.decelSpeed = 0.85;
 
+        this.jumping = false;
+        this.jumpHeight = -100;
+        this.jumpTime = 0;
+        this.maxJumpDuration = 0.16;
+
+        this.directionNum = 1;
+        this.fastFallBoost = 200;
+
+        this.canFastFall = true;
+        this.fastFalling = false;
+        this.fastFallTime = 0;
+        this.fastFallCooldown = 0.05;
+
+        this.externalForceX = 0;
+        this.externalForceY = 0;
+        this.externalForceDecay = 0.90;
     }
 
-    update() {
-        //player movement code
-        if (this.keys.LEFT.isDown) {
+    update(time,delta) {
+        let dt = delta / 1000;
+
+        this.externalForceX = this.externalForceX * this.externalForceDecay
+        if (Math.abs(this.externalForceX) < 1) {
+            this.externalForceX = 0;
+        }
+        //running code
+        this.anims.play('idle',true)
+        if (this.keys.LEFT.isDown && !this.keys.RIGHT.isDown) {
             this.setFlipX(true);
+            this.directionNum = -1;
             let targetVelocity = (Math.max(this.body.velocity.x - this.acceleration, -this.topSpeed));
             this.setVelocityX(targetVelocity);
-            this.anims.play('subaru_run',true);
-        } else if (this.keys.RIGHT.isDown) {
+        } else if (this.keys.RIGHT.isDown && !this.keys.LEFT.isDown) {
             this.setFlipX(false);
+            this.directionNum = 1;
             let targetVelocity = (Math.min(this.body.velocity.x + this.acceleration, this.topSpeed));
             this.setVelocityX(targetVelocity);
-            this.anims.play('subaru_run',true);
         } else {
             let currentVelocity = this.body.velocity.x * this.decelSpeed;
             if (Math.abs(currentVelocity) < 1) {
                 currentVelocity = 0;
-                this.anims.play('subaru_idle',true);
             }
             this.setVelocityX(currentVelocity);
         }
-
-        if (this.keys.UP.isDown){
-            this.setVelocityY(this.jumpHeight);
-            this.anims.play('subaru_jump',false);
+        //jumping code
+        if (this.keys.UP.isDown && this.body.blocked.down && !this.jumping) { 
+            this.jumping = true; 
+            this.body.setVelocityY(this.jumpHeight);
+            this.jumpTime = 0;
+        } else if (this.jumping && this.body.velocity.y < 0) { 
+            this.jumpTime += dt;
+            if (this.jumpTime < this.maxJumpDuration) { 
+                this.body.setVelocityY(this.jumpHeight); 
+            } else {
+                this.jumping = false;
+            }
         }
-        
-        if (this.body.velocity.y > 0) {
-            this.anims.play('subaru_fall',true);
+        //fast-fall code
+        if (this.keys.SHIFT.isDown && !this.body.blocked.down && this.canFastFall) {
+            this.fastFalling = true;
+            this.externalForceX += this.fastFallBoost * this.directionNum
+            this.body.setVelocityY(this.scene.physics.world.gravity.y)
+        } else if (this.fastFalling && this.body.blocked.down) {
+            this.fastFalling = false;
+            this.canFastFall = false;
+            this.fastFallTime = 0;
+        }
+        if (!this.canFastFall) {
+            this.fastFallTime += dt;
+            if (this.fastFallTime >= this.fastFallCooldown) {
+                this.canFastFall = true;
+            }
         }
 
-        //if (this.body.blocked.down && this.body.velocity.y === 0) {
-            //this.anims.play('subaru_land',true);
-        //}
+        if (this.keys.UP.isUp) { 
+            this.jumping = false; 
+        }
+        if (this.body.blocked.down) { 
+            this.jumpTime = 0; 
+        }
     }
 }
